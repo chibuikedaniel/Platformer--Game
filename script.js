0ed94b1ac0b865f4fa1ce5bdd6881fcfaec2f8dc
dracuzi <danielchibuike77@gmail.com>
2024-11-11 22:01:29 +0100

    added a function to create a new instance of player and start game

--- script.js
+++ script.js
@@ -51,3 +51,11 @@ class Player {
         };
     };
 };
+
+const player = new Player();
+
+const startGame = () => {
+    canvas.style.display = "block";
+    startScreen.style.display = "none";
+    player.draw()
+}
