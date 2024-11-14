73103834a97180346076cfc4a96eab2b20197d27
dracuzi <danielchibuike77@gmail.com>
2024-11-14 21:10:43 +0100

    added a function to show checkpoint screen

--- script.js
+++ script.js
@@ -211,6 +211,16 @@ const startGame = () => {
     animate()
 };
 
+const showCheckpointScreen = (msg) => {
+    checkpointScreen.style.display = "block";
+    checkpointMessage.textContent = msg;
+    if (isCheckpointCollisionDetectionActive) {
+        setTimeout(() => {
+            checkpointScreen.style.display = "none";
+        }, 2000);
+    }
+}
+
 startBtn.addEventListener("click", startGame);
 window.addEventListener("keydown", ({ key }) => {
     movePlayer(key, 8, true)
